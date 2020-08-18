5e28c3ade1a2c2a42457203f2c08967e0c94d43a
interested-mandrake <ssepanski343@gmail.com>
2020-08-17 21:38:19 -0500

    add problem description

--- merge-sorted-arrays.java
+++ merge-sorted-arrays.java
@@ -1,10 +1,12 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    // method parameter info: where m is the length of numbers in nums1, n is the length of numbers in nums2, and nums1 has enough capacity to hold all the numbers in both nums1 and nums2
+    public void merge(int[] nums1, int m, int[] nums2, int n) { 
         
 	int [] nums1clone = nums1.clone();
         
         int p = 0; // pointer to nums1, our final output
-        int p1 = 0; // pointer to our copy of nums1, from which we get nums1values
+        int p1 = 0; // pointer to our copy of nums1, from which we get nums1 values
         int p2 = 0; // pointer to nums2, from which we get nums2 values
         
         for(; p1 < m && p2 < n; p++) // perform place until we reach the end of one of the input arrays
